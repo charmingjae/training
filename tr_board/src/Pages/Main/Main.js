6cf44179d6349a97baf2e61bab85ed7f0e19b768
chaminjae <beingeskimo@gmail.com>
2021-07-08 23:40:56 +0900

    [UPDATE]

--- tr_board/src/Pages/Main/Main.js
+++ tr_board/src/Pages/Main/Main.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import {
+  divWrapMainContent,
+  divMainContent,
+  contentTheme,
+  contentSubTheme,
+  contentRemainTheme,
+  contentCountUmb,
+} from "./Main.module.css";
+
 function Main() {
   return (
-    <div>
-      <h1>Hello! This is Main</h1>
+    <div className={`${divWrapMainContent}`}>
+      <div className={`${divMainContent}`}>
+        <h1 className={`${contentTheme}`}>Basket</h1>
+        <h2 className={`${contentSubTheme}`}>Umbrella Rental System</h2>
+        <h1 className={`${contentRemainTheme}`}>Remain Umbrella</h1>
+        <h2 className={`${contentCountUmb}`}>30</h2>
+      </div>
     </div>
   );
 }
